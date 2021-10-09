2ea49cfc5844f950c57a4e9eb5af36f172775820
FredBill1 <1781990573@qq.com>
2021-10-09 13:35:10 +0800

    lol

--- src/test_arm_move/scripts/test_arm_move.py
+++ src/test_arm_move/scripts/test_arm_move.py
@@ -16,17 +16,22 @@ def main():
 
     print("home:", arm.get_pos())
 
-    while True:
-        i = 0
-        while i < 10:
-            x, y, z = (randint(0, 250) for _ in range(3))
-            if arm.check_xyz(x, y, z):
-                print(x, y, z)
-                arm.move_xyz(x, y, z)
-                i += 1
-        arm.move_home()
-        print("sleep\n")
-        sleep(3)
+    arm.move_xyz(100, 100, 0)
+    arm.use(True)
+    arm.move_xyz(250, 0, 50)
+    arm.use(False)
+
+    # while True:
+    #     i = 0
+    #     while i < 10:
+    #         x, y, z = (randint(0, 250) for _ in range(3))
+    #         if arm.check_xyz(x, y, z):
+    #             print(x, y, z)
+    #             arm.move_xyz(x, y, z)
+    #             i += 1
+    #     arm.move_home()
+    #     print("sleep\n")
+    #     sleep(3)
 
 
 if __name__ == "__main__":
