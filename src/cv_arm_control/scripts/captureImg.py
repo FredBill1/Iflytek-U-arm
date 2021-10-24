b875fa812eb10e6bd2a290d4c417d46847e0844f
FredBill1 <1781990573@qq.com>
2021-10-24 17:48:12 +0800

    加入图片采集的脚本

---
+++ src/cv_arm_control/scripts/captureImg.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import os
+
+DIR = "/mnt/sd/trainData"
+TYPE = "meat"
+DELAY = 0
+COUNT = 100
+
+
+class Main:
+    def __init__(self):
+        self.bridge = CvBridge()
+
+    def getImg(self):
+        image = rospy.wait_for_message("/usb_cam/image_rect_color", Image)
+        img = self.bridge.imgmsg_to_cv2(image, "bgr8")
+        img = cv2.flip(img, 1)
+        return img
+
+    def cap(self):
+        d = os.path.join(DIR, TYPE)
+        if not os.path.exists(d):
+            s = input(d + "\n目录不存在，是否创建?(y/N): ")
+            if s != "Y" and s != "y":
+                return
+            os.makedirs(d)
+        N = len(os.listdir(d))
+        for i in range(N, N + COUNT):
+            img = self.getImg()
+            file = os.path.join(d, f"{i:05d}.jpg")
+            print(file)
+            cv2.imwrite(file, img)
+            rospy.sleep(DELAY)
+        print("done.")
+
+
+def main():
+
+    rospy.init_node("test_cv", anonymous=True)
+    m = Main()
+    m.cap()
+
+
+if __name__ == "__main__":
+    main()
