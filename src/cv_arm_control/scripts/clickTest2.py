e1204636ffe99422d07c579a05e6c4f0418f95a8
FredBill1 <1781990573@qq.com>
2021-10-28 20:13:04 +0800

    加入跟随车的clickTest

---
+++ src/cv_arm_control/scripts/clickTest2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import rospy
+from CVArmControl import CVArmControl
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from sensor_msgs.msg import Image
+
+WINNAME = "ClickOnImg"
+
+
+class Main:
+    def init(self) -> None:
+        self.arm = CVArmControl()
+        self.arm.init()
+
+        self.img_sub = rospy.Subscriber("/usb_cam/image_rect_color", Image, self.msgCb)
+        self.cvbridge = CvBridge()
+        cv2.namedWindow(WINNAME)
+        cv2.setMouseCallback(WINNAME, self.clickCb)
+        rospy.on_shutdown(self.on_shutdown)
+        rospy.loginfo("Init done. Click on image to pick a position.")
+
+    def msgCb(self, src: Image) -> None:
+        try:
+            img = self.cvbridge.imgmsg_to_cv2(src, "bgr8")
+            self.img = cv2.flip(img, 1)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            cv2.imshow(WINNAME, self.img)
+            if cv2.waitKey(5) & 0xFF == ord("q"):
+                break
+        rospy.signal_shutdown("pressed 'q'")
+
+    def clickCb(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            rospy.loginfo("\nclick: x:%d y:%d" % (x, y))
+            self.arm.move2d(x, y, 0.18, 0, 50)
+            self.arm.use(True)
+            # rospy.sleep(5)
+            # self.arm.move_xyz_relative(0, 0, 30, 50)
+            # rospy.sleep(5)
+            self.arm.move_home2(200)
+            rospy.sleep(1)
+            self.arm.use(False)
+
+    def on_shutdown(self):
+        cv2.destroyAllWindows()
+        rospy.loginfo("quit.")
+        cv2.destroyAllWindows()
+        rospy.loginfo("quit.")
+
+
+def main():
+    rospy.init_node("clickTest", anonymous=True)
+    m = Main()
+    m.init()
+    m.arm.move_home2()
+    m.spin()
+
+
+if __name__ == "__main__":
+    main()
