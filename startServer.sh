940456762141162da51a2876919eaf0c92ae2cf1
FredBill1 <1781990573@qq.com>
2021-10-30 21:14:30 +0800

    echo

--- startServer.sh
+++ startServer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 gnome-terminal --tab  -- bash -c "roslaunch cv_arm_control imgService.launch"; sleep 3
+echo Yolo launched
 
 gnome-terminal --tab -- bash -c "roslaunch arm_control arm_server.launch"; sleep 3
+echo Arm launched
 
 rosrun cv_arm_control CVArmServer.py
